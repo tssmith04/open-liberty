be1e4ae093cfb15502c0b9326fcf511379cf6ec9
Thomas Smith <tssmith@ibm.com>
2024-07-24 16:28:09 -0400

    Slightly better skeleton

--- dev/com.ibm.ws.webcontainer.servlet.3.1_fat/test-applications/PH62271.jar/src/com/ibm/ws/webcontainer/servlet_31_fat/testservlet31/jar/servlets/PH62271Servlet.java
+++ dev/com.ibm.ws.webcontainer.servlet.3.1_fat/test-applications/PH62271.jar/src/com/ibm/ws/webcontainer/servlet_31_fat/testservlet31/jar/servlets/PH62271Servlet.java
@@ -20,6 +20,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  * This is just a simple Hello World servlet that we can use to drive requests to so that we can
@@ -27,6 +28,7 @@ import javax.servlet.http.HttpServletResponse;
  * of Hello World servlets around.
  */
 @WebServlet("/PH62271Servlet")
+@MultipartConfig(fileSizeThreshold = 50, location = "", maxFileSize = 5000, maxRequestSize = 1000)
 public class PH62271Servlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -52,7 +54,58 @@ public class PH62271Servlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            ServletOutputStream sos1 = response.getOutputStream();
+            Part part = request.getPart("files");
 
+            // Can be shortened eventually, but may be good for debugging at first
+            if (part != null) {
+                ss = "Part        = " + part;
+                sos1.println(ss);
+                ss = "Content 	= " + part.getContentType();
+                sos1.println(ss);
+                ss = "Header  	= " + part.getHeader("files");
+                sos1.println(ss);
+                ss = "HeaderNames = " + part.getHeaderNames();
+                sos1.println(ss);
+
+                Collection<String> headercollection = part.getHeaderNames();
+                for (Iterator<String> It = headercollection.iterator(); It.hasNext();) {
+
+                    String header = It.next();
+                    ss = "Header  	= " + part.getHeader(header);
+                    sos1.println(ss);
+                    ss = "Headers  	= " + part.getHeaders(header);
+                    sos1.println(ss);
+                }
+
+                ss = "InputStream = " + part.getInputStream();
+                sos1.println(ss);
+                ss = "Name        = " + part.getName();
+                sos1.println(ss);
+                ss = "Size        = " + part.getSize();
+                sos1.println(ss);
+
+                sos1.println(" ");
+                ss = "--------  Printing File Content --------";
+                sos1.println(ss);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(part.getInputStream()));
+                String line = null;
+
+                while ((line = in.readLine()) != null) {
+                    sos1.println(line);
+                }
+
+                part.write("/server/file/path/goes/here");
+
+            }
+        } catch (Exception e) {
+            ServletOutputStream sos1 = response.getOutputStream();
+            String ss = "--------  Exception Output --------";
+            sos1.println(ss);
+            sos1.println(e.getMessage());
+        }
     }
 
 }
